7c5aa7a928f58806059a4e94e00a46d44781c2be
Monish Ramadoss <monish.ramadoss@outlook.com>
2018-10-11 02:05:41 -0700

    udate to trainer

--- SentenceGeneratorProject/trainer.py
+++ SentenceGeneratorProject/trainer.py
@@ -4,13 +4,14 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.init as init
 import torch.nn.functional as F
-import json
 from torchtext import vocab
 
 import main
 import sys
 import os
 import numpy as np
+import json
+from multiprocessing import Pool, Queue
 
 parser = main.parser
 GeneratorDevice = main.GeneratorDevice
@@ -31,42 +32,46 @@ validateData = args.validateData
 testData = args.testData
 
 dtype = torch.float32
-
+Max_val = 0
 glove = vocab.GloVe(name='840B', dim=300)
 
 SquadTrainJson = './Dataset/train-v2.0.json'
 SquadTestJson = './Dataset/dev-v2.0.json'
 
 def get_glove_vec(word):
-    return glove.vectors[glove.stoi[word]]
+    try:
+        return glove.vectors[glove.stoi[word]]
+    except:
+        return torch.zeros(300)
 
 def get_word_from_vec(vec, n=10):
     all_dists = [(w,torch.dist(vec, get_glove_vec(w))) for w in glove.itos]
     return sorted(all_dist, key=lambda t: t[1])[:n]
 
-def preProcessTask(Data, title):
+def preProcessTask(Data, title, Max_val):
     TData = Data['data'][title]
     title  = TData['title']
     paragraphs = TData['paragraphs']
     Output = list()
-    
     for context_qas in range(len(paragraphs)):
-        context = paragraphs[context_qas]['context']
+        context = paragraphs[context_qas]['context'].split(r' ')
+        if(Max_val < len(context)):
+            Max_val = len(context)
         qas = paragraphs[context_qas]['qas']
-        Context = [get_glove_vec(word) for word in context.split(r' ')]     
-        Ctx = torch.zeros((1024, 300))
-        Ctx.new_tensor(Context, device=torch.device('cuda:0'))
+        Context = torch.zeros(600, 300)
+        for x in range(len(context)):
+            Context[x] = get_glove_vec(context[x])        
         for qa in range(len(qas)):
             question = qas[qa]['question']
             answers = qas[qa]['answers']
             for ans in range(len(answers)):
-                Ans = torch.zeros((64,300))
+                Answer = torch.zeros(64,300)
                 answer_start = answers[ans]['answer_start']
-                answer_text = answers[ans]['text']
-                Answer_Text = [get_glove_vec(word) for word in answer_text.split(r' ')]  
-                Ans.new_tensor(Answer_Text, device=torch.device('cuda:0'))     
-                Output.append(torch.cat((Ctx, Ans), dim=0))
-    return 
+                answer_text = answers[ans]['text'].split(r' ')
+                for x in range(len(answer_text)):
+                    Answer[x] = get_glove_vec(answer_text[x])
+                Output.append(torch.cat((Context, Answer), dim=0))
+    return Output
                 
 
 def preprocess():
@@ -75,9 +80,14 @@ def preprocess():
     TestData = json.load(open(SquadTestJson))
 
     td = TrainData['data'][0]['paragraphs'][0]['qas']
+    p = Pool(1)    
+    #args = [(TrainData, title, Max_val) for title in range(len(TrainData['data']))]      
+    #Output = p.starmap(preProcessTask, args)
+    #p.join()
 
+    Output = list()
     for title in range(len(TrainData['data'])):
-        preProcessTask(TrainData, title)
+       Output.append(preProcessTask(TrainData, title, Max_val))
                     
     print()
 
