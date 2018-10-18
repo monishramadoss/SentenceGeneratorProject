fab5d15ef4fbd1bdd53162440d4ed4ed44d3ad84
Monish <monish.ramadoss@outlook.com>
2018-10-18 11:51:50 -0700

    update system

--- SentenceGeneratorProject/model.py
+++ SentenceGeneratorProject/model.py
@@ -8,13 +8,18 @@ from torchtext import vocab
 
 
 class SentenceGeneration(nn.Module):
-    def __init__(self, hidden_state=1, num_layer=1, ):
+    def __init__(self, hidden_state=1, num_layers=1, ):
        
         super(SentenceGeneration, self).__init__()
-        self.bi_rnn_encoder = torch.nn.GRU(input_size=300, hidden_state=1, num_layers=1, batch_first=False, bidirectional=True)
-        self.rnn_decoder = torch.nn.GRU(input_size=300, hidden_state=1, num_layer=1, batch_first=False, bidirectional=False)
+        self.bi_rnn_encoder = torch.nn.GRU(input_size=300, hidden_state=hidden_state, num_layers=num_layers, batch_first=False, bidirectional=True)
+        self.rnn_decoder = torch.nn.GRU(input_size=300, hidden_state=hidden_state, num_layer=num_layers, batch_first=False, bidirectional=False)
 
 
     def forward(self, x):
-        pass
+        
+        Context_featureMap = self.bi_rnn_encoder(x)
+        Question_Decoding = self.rnn_decoder(Context_featureMap)
+
+    return Question_Decoding
+
 
