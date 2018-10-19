518c3dd54eb6f649a7294e16112c65f01377c8de
Monish Ramadoss <monish.ramadoss@outlook.com>
2018-10-19 01:54:20 -0700

    Update Trainer

--- SentenceGeneratorProject/trainer.py
+++ SentenceGeneratorProject/trainer.py
@@ -49,10 +49,12 @@ StanfordParser = ['https://nlp.stanford.edu/software/stanford-ner-2018-02-27.zip
                   'https://nlp.stanford.edu/software/stanford-postagger-2018-02-27.zip',
                   'https://nlp.stanford.edu/software/stanford-parser-full-2018-02-27.zip']
 
-
-
-
 tags = {'CC':0, 'CD':1, 'DT':2, 'EX':3, 'FW':4, 'IN':5, 'JJ':6, 'JJR':7, 'JJS':8, 'LS':9, 'MD':10, 'NN':11, 'NNS':12, 'NNP':13, 'NNPS':14, 'PDT':15, 'POS':16, 'PRP':17, 'PRP$':18, 'RB':19, 'RBR': 20, 'RBS':21, 'RP':22, 'SYM':23, 'TO':24, 'UH':25, 'VB':26, 'VBD':27, 'VBG':28, 'VBN':29, 'VBP':30, 'VBZ':31, 'WDT':32, 'WP':33, 'WP$':34, 'WRB':35}
+
+SentenceLength = 48
+ContextLength = 800
+QuestionLength = 32
+AnswerLength = 16
 
 def get_glove_vec(word):
     try:
@@ -71,8 +73,8 @@ def preProcessTask(Data, title, Max_val):
     Output = list()
     imposobru = False
     for context_qas in range(len(paragraphs)):
-        Tags = torch.zeros(800,36)
-        Context = torch.zeros(800, 300)
+        Tags = torch.zeros(ContextLength, 36)
+        Context = torch.zeros(ContextLength, 300)
         ctx = paragraphs[context_qas]['context']
         ctx_sents = ctx.split(r'.')
         context =  nltk.word_tokenize(ctx)
@@ -88,68 +90,79 @@ def preProcessTask(Data, title, Max_val):
 
         nerResult = nltk.ne_chunk(nltk.pos_tag(context))
         bioTagged = list()
-        prevTag = 0
-
-       
+        prevTag = 0      
             
-        Context = torch.cat((Context,Tags), dim=0)
+        Context = torch.cat((Context,Tags), dim=1)
         
         if(Max_val < len(context)):
             Max_val = len(context)
 
         qas = paragraphs[context_qas]['qas']
         for qa in range(len(qas)):
-            Question = torch.zeros(64,300)
-            Tags = torch.zeros(64,36)
+            Question = torch.zeros(QuestionLength,300)
+            Tags = torch.zeros(QuestionLength, 36)
 
             question = nltk.word_tokenize(qas[qa]['question'])
             tagged = [tags[x[1]] if x[1] in tags.keys() else 36 for x in nltk.pos_tag(question)  ]        
 
             for x in range(len(question)):
-                Question[x] = get_glove_vec(question[x])                
-                    
+                try:
+                    Question[x] = get_glove_vec(question[x])                
+                except:
+                    break
+
             for x in range(len(tagged)):
-                tmp = tagged[x]
-                if(tmp != 36 ):
-                    Tags[x][tmp] = 1
-            
-            Question = torhch.cat((Question, Tags), dim=0)
+                try:
+                    tmp = tagged[x]
+                    if(tmp != 36 ):
+                        Tags[x][tmp] = 1
+                except:
+                    pass
+
+            Question = torch.cat((Question, Tags), dim=1)
 
             answers = qas[qa]['answers']
             imposobru = qas[qa]['is_impossible']
             sentence = ""
-            Sentence = torch.zeros(64,300)
-            Tags = torch.zeros(64,36)
+            Sentence = torch.zeros(SentenceLength, 300)
+            Tags = torch.zeros(SentenceLength, 36)
             if(not imposobru):
                 for ans in range(len(answers)):
-                    Answer = torch.zeros(64,300)
+                    Answer = torch.zeros(AnswerLength, 300)
                     answer_start = answers[ans]['answer_start']
                     answer_text = answers[ans]['text'].split(r' ')
                     
                     for sent in ctx_sents:
-                        if(answer_text in sent):
+                        if(' '.join(answer_text) in sent):
                             sentence = nltk.word_tokenize(sent)
                             for x in range(len(sentence)):
-                                Sentence = get_glove_vec(sentence[x])
+                                try:
+                                    Sentence[x]= get_glove_vec(sentence[x])
+                                except:
+                                    break
+
                             tagged = [tags[x[1]] if x[1] in tags.keys() else 36 for x in nltk.pos_tag(sentence)]    
                             for x in range(len(tagged)):
-                                tmp = tagged[x]
-                                if(tmp != 36):
-                                    Tags[x][tmp] = 1
-                                    
-                                
+                                try:
+                                    tmp = tagged[x]
+                                    if(tmp != 36):
+                                        Tags[x][tmp] = 1                                                                 
+                                except:
+                                    break
+                            Sentence = torch.cat((Sentence, Tags), dim=1)
                             break 
 
                     for x in range(len(answer_text)):
-                        Answer[x] = get_glove_vec(answer_text[x])
-                    
-                    Output.append((Context, Question, Answer))
-
-    if(not imposobru):
-        result = torch.cat(Output, dim=0)
-        return (result, True) 
+                        try:
+                            Answer[x] = get_glove_vec(answer_text[x])                      
+                        except:
+                            break
+                    Output.append((Context, Sentence, Question, Answer))
+    
+    if(imposobru == False):
+        return (Output, False)
     else:
-        return (None, False)
+        return (None, True)
    
 def preprocess():
     
@@ -167,13 +180,8 @@ def preprocess():
        tmp, Flag = preProcessTask(TrainData, title, Max_val)
        if(Flag):
            Output.append(tmp)
-
-    Output = torch.tensor(torch.cat(Output, dim=0))
-
     
-    nparray = Output.numpy()
-    np.savetxt('./TrainData.gz', nparray)
-    print()
+    return Output    
 
 def train():
     preprocess()
